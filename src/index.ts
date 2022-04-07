2ab1d1ec4c8bcdb47f7b5aa191e888a0cde90a1b
Alex Blokh <aleksandrblokh@gmail.com>
2022-04-07 15:38:46 +0300

    Support parametrized queries

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import { QueryResult } from "pg";
 import { ISession } from 'drizzle-orm/db/session'
+import { Knex } from 'knex';
 
 export class KnexSession extends ISession {
-    public constructor(private knex: any) {
+    public constructor(private knex: Knex) {
         super();
     }
 
-    public execute(query: string): Promise<QueryResult<any>> {
-        return this.knex.raw(query);
+    public execute(query: string, values?: Array<any>): Promise<QueryResult<any>> {
+        return this.knex.raw(query, values ?? [])
+    }
+
+    parametrized(_: number): string {
+       return "?"
     }
 }
